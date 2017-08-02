f054e83684d69de5903fd90709d5e77cc8b72d6d
Jason Voegele <jason.voegele@gmail.com>
2017-08-02 12:43:45 -0400

    Initial import of existing code

---
+++ lib/external_service.ex
@@ -0,0 +1,197 @@
+defmodule ExternalService do
+  @moduledoc """
+  ExternalService handles all retry and circuit breaker logic for calls to external services.
+  """
+
+  use Retry
+  alias :fuse, as: Fuse
+
+  defmodule RetryOptions do
+    @moduledoc """
+    Options used for controlling retry logic.
+    See the [retry docs](https://hexdocs.pm/retry/Retry.html) for information about the available
+    retry options.
+    """
+
+    defstruct backoff: {:exponential, 10},
+              randomize: false,
+              expiry: nil,
+              cap: nil
+    @type t :: %__MODULE__{
+      backoff: {:exponential, pos_integer()} | {:linear, pos_integer(), pos_integer()},
+      randomize: boolean(),
+      expiry: pos_integer() | nil,
+      cap: pos_integer() | nil
+    }
+  end
+
+  @type fuse_name :: atom()
+  @type retriable_function_result :: :retry | {:retry, reason::any()} | function_result::any()
+  @type retriable_function :: (() -> retriable_function_result())
+
+  @typedoc """
+  Options used for controlling circuit breaker behavior.
+  See the [fuse docs](https://hexdocs.pm/fuse/) for information about available fuse options.
+  """
+  @type fuse_options :: [
+    fuse_strategy: {:standard, pos_integer(), pos_integer()},
+    fuse_refresh: pos_integer()
+  ]
+
+  @default_fuse_options %{
+    fuse_strategy: {:standard, 10, 10_000},
+    fuse_refresh: 60_000
+  }
+
+  @doc """
+  Initializes a new fuse for a given service.
+  """
+  @spec start(fuse_name(), fuse_options()) :: :ok
+  def start(fuse_name, fuse_options \\ []) do
+    fuse_opts = {
+      Keyword.get(fuse_options, :fuse_strategy, @default_fuse_options.fuse_strategy),
+      {:reset, Keyword.get(fuse_options, :fuse_refresh, @default_fuse_options.fuse_refresh)}
+    }
+    :ok = Fuse.install(fuse_name, fuse_opts)
+  end
+
+  @doc """
+  Given a fuse name and retry options execute a function handling any retry and circuit breaker
+  logic. `ExternalService.start` must be run with the fuse name before using call.
+
+  The provided function can indicate that a retry should be performed by returning the atom
+  `:retry` or a tuple of the form `{:retry, reason}`, where `reason` is any arbitrary term, or by
+  raising a `RuntimeError`. Any other result is considered successful so the operation will not be
+  retried and the result of the function will be returned as the result of `call`.
+  """
+  @spec call(fuse_name(), RetryOptions.t, retriable_function()) ::
+    {:error, {:retries_exhausted, reason::any()}} |
+    {:error, {:fuse_blown, fuse_name()}} |
+    {:error, {:fuse_not_found, fuse_name()}} |
+    function_result::any()
+  def call(fuse_name, retry_opts \\ %RetryOptions{}, function) do
+    case do_retry(fuse_name, retry_opts, function) do
+      {:no_retry, result} -> result
+      {:error, :retry} -> {:error, {:retries_exhausted, :reason_unknown}}
+      {:error, {:retry, reason}} -> {:error, {:retries_exhausted, reason}}
+      result -> result
+    end
+  end
+
+  @doc """
+  An async version of `ExternalService.call`. Returns a `Task` that may be used to retrieve the
+  result of the async call.
+  """
+  @spec call_async(fuse_name(), RetryOptions.t, retriable_function()) :: Task.t
+  def call_async(fuse_name, retry_opts \\ %RetryOptions{}, function) do
+    Task.async(fn -> call(fuse_name, retry_opts, function) end)
+  end
+
+  @doc """
+  A parallel, streaming version of `ExternalService.call`.
+
+  See `call_async_stream/5` for full documentation.
+  """
+  @spec call_async_stream(Enumerable.t,
+                          fuse_name(),
+                          ((any()) -> retriable_function_result())) :: Enumerable.t
+  def call_async_stream(enumerable, fuse_name, function) when is_function(function),
+    do: call_async_stream(enumerable, fuse_name, %RetryOptions{}, [], function)
+
+  @doc """
+  A parallel, streaming version of `ExternalService.call`.
+
+  See `call_async_stream/5` for full documentation.
+  """
+  @spec call_async_stream(Enumerable.t,
+                          fuse_name(),
+                          RetryOptions.t | async_opts::list(),
+                          ((any()) -> retriable_function_result())) :: Enumerable.t
+  def call_async_stream(enumerable, fuse_name, retry_opts = %RetryOptions{}, function)
+  when is_function(function),
+    do: call_async_stream(enumerable, fuse_name, retry_opts, [], function)
+  def call_async_stream(enumerable, fuse_name, async_opts, function)
+  when is_list(async_opts) and is_function(function),
+    do: call_async_stream(enumerable, fuse_name, %RetryOptions{}, async_opts, function)
+
+  @doc """
+  A parallel, streaming version of `ExternalService.call`. This function uses Elixir's built-in
+  `Task.async_stream/3` function and the description below is taken from there.
+
+  Returns a stream that runs the given function `function` concurrently on each
+  item in `enumerable`.
+
+  Each `enumerable` item is passed as argument to the given function `function`
+  and processed by its own task. The tasks will be linked to the current
+  process, similarly to `async/1`.
+  """
+  @spec call_async_stream(Enumerable.t,
+                          fuse_name(),
+                          RetryOptions.t,
+                          async_opts::list(),
+                          ((any()) -> retriable_function_result())) :: Enumerable.t
+  def call_async_stream(enumerable,
+                        fuse_name,
+                        retry_opts = %RetryOptions{},
+                        async_opts,
+                        function)
+  when is_list(async_opts) and is_function(function) do
+    fun = fn(item) ->
+      call(fuse_name, retry_opts, fn -> function.(item) end)
+    end
+    Task.async_stream(enumerable, fun, async_opts)
+  end
+
+  @spec do_retry(fuse_name(), RetryOptions.t, retriable_function()) ::
+    {:no_retry, function_result::any()} |
+    {:error, :retry} |
+    {:error, {:retry, reason::any()}} |
+    function_result::any()
+  defp do_retry(fuse_name, retry_opts, function) do
+    retry(with: apply_retry_options(retry_opts)) do
+      case Fuse.ask(fuse_name, :sync) do
+        :ok -> try_function(fuse_name, function)
+        :blown -> throw :blown
+        {:error, :not_found} -> throw :not_found
+      end
+    end
+  catch
+    :blown -> {:error, {:fuse_blown, fuse_name}}
+    :not_found -> {:error, {:fuse_not_found, fuse_name}}
+  end
+
+  defp apply_retry_options(retry_opts) do
+    delay_stream = case retry_opts.backoff do
+      {:exponential, initial_delay} -> exp_backoff(initial_delay)
+      {:linear, initial_delay, factor} -> lin_backoff(initial_delay, factor)
+    end
+    retry_opts
+    |> Map.take([:randomize, :expiry, :cap])
+    |> Enum.reduce(delay_stream, fn({key, value}, acc) ->
+      if value do
+        apply(Retry.DelayStreams, key, [acc, value])
+      else
+        acc
+      end
+    end)
+  end
+
+  @spec try_function(atom(), (() -> any())) ::
+    {:error, {:retry, any()}} | {:error, :retry} | {:no_retry, any()}
+  defp try_function(fuse_name, function) do
+    case function.() do
+      {:retry, reason} ->
+        Fuse.melt(fuse_name)
+        {:error, {:retry, reason}}
+      :retry ->
+        Fuse.melt(fuse_name)
+        {:error, :retry}
+      result ->
+        {:no_retry, result}
+    end
+    rescue
+      error ->
+        Fuse.melt(fuse_name)
+        reraise error, System.stacktrace()
+  end
+end
---
+++ test/external_service_test.exs
@@ -0,0 +1,171 @@
+defmodule ExternalServiceTest do
+  use ExUnit.Case
+  alias ExternalService
+  alias ExternalService.RetryOptions
+
+  @fuse_name :"test-fuse"
+
+  @retry_opts %RetryOptions{
+    backoff: {:linear, 0, 1}
+  }
+
+  @expiring_retry_options %RetryOptions{
+    backoff: {:linear, 1, 1},
+    expiry: 1,
+  }
+
+  describe "start" do
+    test "installs a fuse" do
+      ExternalService.start(@fuse_name)
+      assert :ok = :fuse.ask(@fuse_name, :sync)
+    end
+  end
+
+  describe "call" do
+    @fuse_retries 5
+
+    setup do
+      Process.put(@fuse_name, 0)
+      ExternalService.start(@fuse_name, fuse_strategy: {:standard, @fuse_retries, 10_000})
+    end
+
+    test "calls function once when successful" do
+      ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        Process.put(@fuse_name, Process.get(@fuse_name) + 1)
+        :ok
+      end)
+      assert 1 = Process.get(@fuse_name)
+    end
+
+    test "calls function again when it returns retry" do
+      ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        Process.put(@fuse_name, Process.get(@fuse_name) + 1)
+        :retry
+      end)
+      assert @fuse_retries + 1 == Process.get(@fuse_name)
+    end
+
+    test "stops retrying on success" do
+      ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        Process.put(@fuse_name, Process.get(@fuse_name) + 1)
+        case Process.get(@fuse_name) do
+          1 -> :retry
+          _ -> :ok
+        end
+      end)
+      assert 2 == Process.get(@fuse_name)
+    end
+
+    test "calls function again when it raises an exception" do
+      ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        Process.put(@fuse_name, Process.get(@fuse_name) + 1)
+        raise "KABOOM!"
+      end)
+      assert @fuse_retries + 1 == Process.get(@fuse_name)
+    end
+
+    test "returns fuse_blown when the fuse is blown by retries" do
+      res = ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        :retry
+      end)
+      assert {:error, {:fuse_blown, @fuse_name}} = res
+    end
+
+    test "returns fuse_blown when the fuse is blown by exceptions" do
+      res = ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        raise "KABOOM!"
+      end)
+      assert {:error, {:fuse_blown, @fuse_name}} = res
+    end
+
+    test "returns :error when retries are exhausted with :retry" do
+      res = ExternalService.call(@fuse_name, @expiring_retry_options, fn() ->
+        :retry
+      end)
+      assert {:error, {:retries_exhausted, :reason_unknown}} = res
+    end
+
+    test "returns :error when retries are exhausted with a reason" do
+      res = ExternalService.call(@fuse_name, @expiring_retry_options, fn() ->
+        {:retry, "reason"}
+      end)
+      assert {:error, {:retries_exhausted, "reason"}} = res
+    end
+
+    test "returns :error when retries are exhausted by an exception" do
+      assert_raise RuntimeError, "KABOOM!", fn() ->
+        ExternalService.call(@fuse_name, @expiring_retry_options, fn() ->
+          raise "KABOOM!"
+        end)
+      end
+    end
+
+    test "returns :error when not retriable" do
+      res = ExternalService.call(@fuse_name, @retry_opts, fn() ->
+        {:error, "reason"}
+      end)
+      assert {:error, "reason"} = res
+    end
+  end
+
+  describe "call_async" do
+    setup do
+      ExternalService.start(@fuse_name, fuse_strategy: {:standard, @fuse_retries, 10_000})
+    end
+
+    test "returns a Task" do
+      task = ExternalService.call_async(@fuse_name, fn -> :ok end)
+      assert :ok = Task.await(task)
+    end
+  end
+
+  describe "call_async_stream" do
+    setup do
+      ExternalService.start(@fuse_name, fuse_strategy: {:standard, @fuse_retries, 10_000})
+    end
+
+    def function(:raise), do: raise "KABOOM!"
+    def function(arg), do: arg
+
+    @enumerable [42, :ok, :retry, :error, {:error, :reason}, :raise]
+
+    test "with no options" do
+      results =
+        @enumerable
+        |> ExternalService.call_async_stream(@fuse_name, &function/1)
+        |> Enum.to_list()
+      assert [{:ok, _}, {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}, {:ok, _},
+             {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}] = results
+    end
+
+    test "with retry options" do
+      results =
+        @enumerable
+        |> ExternalService.call_async_stream(@fuse_name, @retry_opts, &function/1)
+        |> Enum.to_list()
+      assert [{:ok, _}, {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}, {:ok, _},
+             {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}] = results
+    end
+
+    @async_opts [max_concurrency: 100, timeout: 10_000]
+
+    test "with async options" do
+      results =
+        @enumerable
+        |> ExternalService.call_async_stream(@fuse_name, @async_opts, &function/1)
+        |> Enum.to_list()
+      assert [{:ok, _}, {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}, {:ok, _},
+             {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}] = results
+    end
+
+    test "with retry and async options" do
+      results =
+        @enumerable
+        |> ExternalService.call_async_stream(@fuse_name, @retry_opts, @async_opts, &function/1)
+        |> Enum.to_list()
+      assert [{:ok, _}, {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}, {:ok, _},
+             {:ok, _}, {:ok, {:error, {:fuse_blown, :"test-fuse"}}}] = results
+    end
+
+  end
+end
