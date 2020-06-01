3ef16ba783108a7010a1fdf06b324309569a777b
Jason Voegele <jason.voegele@gmail.com>
2020-06-01 09:48:19 -0400

    Extract RetryOptions into own module

--- lib/external_service.ex
+++ lib/external_service.ex
@@ -3,9 +3,11 @@ defmodule ExternalService do
   ExternalService handles all retry and circuit breaker logic for calls to external services.
   """
 
-  require Logger
+  alias ExternalService.RetryOptions
   alias ExternalService.RateLimit
   alias :fuse, as: Fuse
+
+  require Logger
 
   @typedoc "Name of a fuse"
   @type fuse_name :: atom()
@@ -57,52 +59,6 @@ defmodule ExternalService do
     fuse_strategy: {:standard, 10, 10_000},
     fuse_refresh: 60_000
   }
-
-  defmodule RetryOptions do
-    @moduledoc """
-    Options used for controlling retry logic.
-    See the [retry docs](https://hexdocs.pm/retry/Retry.html) for information about the available
-    retry options.
-    """
-
-    @typedoc """
-    A tuple describing the backoff strategy for increasing delay between retries.
-
-    The first element of the tuple must be one of the atoms `:exponential` or `:linear`.
-    In both cases, the second element of the tuple is an integer representing the initial delay
-    between retries, in milliseconds.
-    For linear delay, there is also a third element in the tuple, which is a number representing
-    the factor that the initial delay will be multiplied by on each successive retry.
-    """
-    @type backoff ::
-            {:exponential, initial_delay :: pos_integer()}
-            | {:linear, initial_delay :: pos_integer(), factor :: pos_integer()}
-
-    @typedoc """
-    Struct representing the retry options to apply to calls to external services.
-
-    - `backoff`: tuple describing the backoff strategy (see `t:backoff/0`)
-    - `randomize`: boolean indicating whether or not delays between retries should be randomized
-    - `expiry`: limit total length of time to allow for retries to the specified time budget
-        milliseconds
-    - `cap`: limit maximum amount of time between retries to the specified number of milliseconds
-    - `rescue_only`: retry only on exceptions matching one of the list of provided exception types,
-        (defaults to `[RuntimeError]`)
-    """
-    @type t :: %__MODULE__{
-            backoff: backoff(),
-            randomize: boolean(),
-            expiry: pos_integer() | nil,
-            cap: pos_integer() | nil,
-            rescue_only: list(module())
-          }
-
-    defstruct backoff: {:exponential, 10},
-              randomize: false,
-              expiry: nil,
-              cap: nil,
-              rescue_only: [RuntimeError]
-  end
 
   defmodule RetriesExhaustedError do
     @moduledoc """
@@ -249,6 +205,8 @@ defmodule ExternalService do
           RetryOptions.t() | (async_opts :: list()),
           (any() -> retriable_function_result())
         ) :: Enumerable.t()
+  def call_async_stream(enumerable, fuse_name, retry_opts_or_async_opts, function)
+
   def call_async_stream(enumerable, fuse_name, retry_opts = %RetryOptions{}, function)
       when is_function(function),
       do: call_async_stream(enumerable, fuse_name, retry_opts, [], function)
---
+++ lib/external_service/retry_options.ex
@@ -0,0 +1,50 @@
+defmodule ExternalService.RetryOptions do
+  @moduledoc """
+  Options used for controlling retry logic.
+  See the [retry docs](https://hexdocs.pm/retry/Retry.html) for information about the available
+  retry options.
+  """
+
+  @typedoc """
+  A tuple describing the backoff strategy for increasing delay between retries.
+
+  The first element of the tuple must be one of the atoms `:exponential` or `:linear`.
+  In both cases, the second element of the tuple is an integer representing the initial delay
+  between retries, in milliseconds.
+  For linear delay, there is also a third element in the tuple, which is a number representing
+  the factor that the initial delay will be multiplied by on each successive retry.
+  """
+  @type backoff ::
+  {:exponential, initial_delay :: pos_integer()}
+  | {:linear, initial_delay :: pos_integer(), factor :: pos_integer()}
+
+  @typedoc """
+  Struct representing the retry options to apply to calls to external services.
+
+    * `backoff`: tuple describing the backoff strategy (see `t:backoff/0`)
+    * `randomize`: boolean indicating whether or not delays between retries should be randomized
+    * `expiry`: limit total length of time to allow for retries to the specified time budget
+    *   milliseconds
+    * `cap`: limit maximum amount of time between retries to the specified number of milliseconds
+    * `rescue_only`: retry only on exceptions matching one of the list of provided exception types,
+        (defaults to `[RuntimeError]`)
+  """
+  @type t :: %__MODULE__{
+    backoff: backoff(),
+    randomize: boolean(),
+    expiry: pos_integer() | nil,
+    cap: pos_integer() | nil,
+    rescue_only: list(module())
+  }
+
+  defstruct backoff: {:exponential, 10},
+  randomize: false,
+  expiry: nil,
+  cap: nil,
+  rescue_only: [RuntimeError]
+
+  def new(opts) do
+    struct(__MODULE__, opts)
+  end
+end
+
